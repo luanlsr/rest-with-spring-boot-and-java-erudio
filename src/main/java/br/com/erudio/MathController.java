3f243536ef6cc756d4eb313cb72e3c660501a8c5
Luan Ramalho <luanfswd@gmail.com>
2022-07-25 20:06:47 -0300

    structure of sum controller

---
+++ src/main/java/br/com/erudio/MathController.java
@@ -0,0 +1,32 @@
+package br.com.erudio;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MathController {
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
+            method = RequestMethod.GET)
+    public Double sum(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo)
+    throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new Exception();
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        // TODO
+        return null;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        // TODO
+        return false;
+    }
+}
